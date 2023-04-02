2ed66fb071a42ae95d467886d7786179d6f09f61
PSCostaM <pscostam11@gmail.com>
2023-04-01 21:15:04 -0500

    Testeo

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
+import OneSignal from 'onesignal-cordova-plugin';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [IonicModule],
+})
+export class AppComponent {
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      OneSignalInit();
+    });
+  }
+
+}
+function OneSignalInit(): void {
+  // Uncomment to set OneSignal device logging to VERBOSE  
+  // OneSignal.setLogLevel(6, 0);
+
+  // NOTE: Update the setAppId value below with your OneSignal AppId.
+  OneSignal.setAppId("bd875ffc-b5df-49c1-b80f-797822d987bd");
+  OneSignal.setNotificationOpenedHandler(function(jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  });
+
+  // Prompts the user for notification permissions.
+  //    * Since this shows a generic native prompt, we recommend instead using an In-App Message to prompt for notification permission (See step 7) to better communicate to your users what notifications they will get.
+  OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+      console.log("User accepted notifications: " + accepted);
+  });
+}
